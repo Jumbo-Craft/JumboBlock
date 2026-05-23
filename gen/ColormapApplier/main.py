f3539837e2b16d08b0b5ee10f9a08a4b97e87009
Smile-NS <nssmile488@gmail.com>
2026-05-23 19:52:15 +0900

    add: merge grass block side & its overlay

--- gen/ColormapApplier/main.py
+++ gen/ColormapApplier/main.py
@@ -31,6 +31,10 @@ def main():
 
                 r,g,b,a = colormap.getpixel((int(x), int(y)))
                 output = apply_colors(Image.open(f'./input/block/{img}').convert('RGBA'), (r, g, b, a))
+
+                if img == 'grass_block_side_overlay.png':
+                    output = merge_grass_block_side(Image.open(f'./input/grass_block_side.png').convert('RGBA'), output)
+
                 biome_name = os.path.splitext(biome)[0]
                 os.makedirs(f'./output/{biome_name}', exist_ok=True)
                 path = f'./output/{biome_name}/{img}'
@@ -49,6 +53,13 @@ def apply_colors(img, base_color):
             img.putpixel((x, y), (int(r), int(g), int(b), a))
     return img
 
+def merge_grass_block_side(img, overlay):
+    for x in range(overlay.size[0]):
+        for y in range(overlay.size[1]):
+            rgba = overlay.getpixel((x, y))
+            if rgba[3] == 255:
+                img.putpixel((x, y), rgba)
+    return img
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
